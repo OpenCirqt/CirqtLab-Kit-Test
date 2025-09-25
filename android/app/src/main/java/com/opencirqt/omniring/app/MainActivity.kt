eec7ccea676ae2bb782b7f7a9174adfd6db43417
Shengming Yuan <dev@theeditorstudio.com>
2025-09-25 12:55:08 -0400

    update ios

--- android/app/src/main/java/com/opencirqt/omniring/app/MainActivity.kt
+++ android/app/src/main/java/com/opencirqt/omniring/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.opencirqt.omniRing
+package com.opencirqt.omniring.app
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- android/app/src/main/java/com/opencirqt/omniring/app/MainApplication.kt
+++ android/app/src/main/java/com/opencirqt/omniring/app/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.opencirqt.omniRing
+package com.opencirqt.omniring.app
 
 import android.app.Application
 import android.content.res.Configuration
